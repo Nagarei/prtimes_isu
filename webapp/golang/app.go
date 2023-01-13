af0dedde2bf93dcc41a102b42f35f658b192733f
Nagarei <Nagarei@users.noreply.github.com>
2023-01-13 17:49:27 +0900

    remove log

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -16,8 +16,6 @@ import (
 	"strings"
 	"time"
 
-	_ "net/http/pprof"
-
 	"github.com/bradfitz/gomemcache/memcache"
 	gsm "github.com/bradleypeabody/gorilla-sessions-memcache"
 	"github.com/go-chi/chi/v5"
@@ -933,9 +931,9 @@ func postAdminBanned(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
+	// go func() {
+	// 	log.Println(http.ListenAndServe("localhost:6060", nil))
+	// }()
 
 	host := os.Getenv("ISUCONP_DB_HOST")
 	if host == "" {

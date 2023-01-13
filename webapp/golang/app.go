361324a9f2ecb33ff394b9b5ed5faf14999f2146
Nagarei <Nagarei@users.noreply.github.com>
2023-01-13 12:52:00 +0900

    limit

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -388,7 +388,12 @@ func getIndex(w http.ResponseWriter, r *http.Request) {
 
 	results := []Post{}
 
-	err := db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` ORDER BY `created_at` DESC")
+	err := db.Select(&results,
+		"SELECT posts.`id`, posts.`user_id`, posts.`body`, posts.`mime`, posts.`created_at` FROM `posts`"+
+			" join users on user.id = posts.user_id"+
+			" where user.del_flg = 0"+
+			" ORDER BY posts.created_at DESC"+
+			" LIMIT ?", postsPerPage)
 	if err != nil {
 		log.Print(err)
 		return
